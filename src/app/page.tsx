230fb6f2cfc630a372725dd707b2d67ecf786880
yude <i@yude.jp>
2023-05-21 09:21:41 +0900

    Fix: ソフトウェアキーボードを閉じた際に一覧表示の高さが更新されない問題を修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useMemo, useEffect, useState, useRef, JSXElementConstructor, PromiseLikeOfReactNode, ReactElement, ReactFragment, ReactPortal } from 'react';
+import { useMemo, useEffect, useState, useRef, JSXElementConstructor, PromiseLikeOfReactNode, ReactElement, ReactFragment, ReactPortal, useCallback } from 'react';
 import { usePapaParse } from 'react-papaparse';
 import { TData } from "@/types/Table";
 
@@ -130,6 +130,35 @@ export default function Home() {
     rowData: books,
   }
 
+  // Workaround for page height updates regarding software keyboards
+  const getHeight = useCallback(
+    () =>
+      window.visualViewport ? window.visualViewport.height : window.innerHeight,
+    [],
+  )
+  const [height, setHeight] = useState<number>(getHeight())
+  useEffect(() => {
+    const handleResize = (e: Event) => {
+      setHeight(getHeight())
+    }
+
+    window.addEventListener('resize', handleResize)
+    // From the top of my head this used to be required for older browsers since
+    // this didn't trigger a resize event. Keeping it in to be safe.
+    window.addEventListener('orientationchange', handleResize)
+    // This is needed on iOS to resize the viewport when the Virtual/OnScreen
+    // Keyboard opens. This does not trigger any other event, or the standard
+    // resize event.
+    window.visualViewport?.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+      window.removeEventListener('orientationchange', handleResize)
+      window.visualViewport?.removeEventListener('resize', handleResize)
+    }
+  }, [getHeight])
+
+
   return (
     <>
     <p className="font-mono bg-pink-200/75 text-4xl tracking-wide underline decoration-pink-500 decoration-4">
@@ -142,7 +171,7 @@ export default function Home() {
     <form className="bg-white rounded px-8 pt-6 pb-4" onSubmit={e => { e.preventDefault(); }}>
       <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="search_query" type="text" value={filterText} onChange={handleFilterUpdate} placeholder="書籍を検索..." />
     </form>
-    <div style={{ height: `${windowSize.height - 230}px`}}>
+    <div style={{ height: `${getHeight() - 230}px`}}>
     {books && gridRef && 
     
     <AgGridReact
